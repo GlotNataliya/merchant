7028fce54fb6761d104841fa5d5b12bbfcad4f25
Nataliya Glotova <an.chishevich@yandex.ru>
2023-03-20 00:57:23 +0300

    Errors notification refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def form_error_notification(object)
+    if object.errors.any?
+      tag.div class: "text-danger rounded-pill bg-light p-3 text-center" do
+        object.errors.full_messages.to_sentence.capitalize
+      end
+    end
+  end
 end
