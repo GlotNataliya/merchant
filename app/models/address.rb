f57f79905690cd3587d778476b19cc5bd84dc8a0
Nataliya Glotova <an.chishevich@yandex.ru>
2023-03-22 18:23:00 +0300

    Add validation for state of country into address

--- app/models/address.rb
+++ app/models/address.rb
@@ -4,7 +4,7 @@ class Address < ApplicationRecord
 
   validates :line1, :city, :state, :zip, presence: true
   validates :zip, numericality: { only_integer: true }, length: { maximum: 5 }
-  validates :state, length: { maximum: 2 }
+  validates :state, length: { maximum: 2 }, format: { with: /([A-Z])/, message: "must have only two capital letters" }
 
   def to_s
     [line1, line2, city, state, zip].select(&:present?).join(', ')
